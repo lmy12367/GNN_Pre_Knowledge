1a31ee585eb26de1c22d5ad8deeb85cb726ba6da
lmy12367 <lfuturemy@163.com>
2025-09-23 21:33:15 +0800

    GCN

---
+++ GCN.py
@@ -0,0 +1,15 @@
+import networkx as nx
+print(nx.__version__)
+
+G=nx.Graph()
+
+G.add_nodes_from(['v1','v2','v3','v4','v5'])
+G.add_edges_from([('v1','v2'),('v1','v3'),('v2','v3'),('v2','v4'),('v3','v4'),('v4','v5')])
+print(nx.is_directed(G))
+print(nx.is_weighted(G))
+
+nx.draw(G,with_labels=True,pos=nx.spring_layout(G))
+
+
+
+
