575c2862f0e82797e2df0ddfb5d2bfeb675c9114
lmy12367 <lfuturemy@163.com>
2025-09-27 16:12:35 +0800

    GCN

--- GCN.py
+++ GCN.py
@@ -1,18 +1,38 @@
-import networkx as nx
-print(nx.__version__)
+import torch
+from torch.nn import Linear,Parameter
+from torch_geometric.nn import MessagePassing
+from torch_geometric.utils import add_self_loops,degree
 
-G=nx.Graph()
+class GCNConv(MessagePassing):
+    def __init__(self,in_channels,out_channels):
+        super().__init__(agg= "add")
+        self.lin=Linear(in_channels,out_channels,bias=False)
+        self.bias=Parameter(torch.empty(out_channels))
 
-G.add_nodes_from(['v1','v2','v3','v4','v5'])
-G.add_edges_from([('v1','v2'),('v1','v3'),('v2','v3'),('v2','v4'),('v3','v4'),('v4','v5')])
-print(nx.is_directed(G))
-print(nx.is_weighted(G))
+        self.reset_parameters()
 
-G1=nx.draw(G,with_labels=True,pos=nx.spring_layout(G))
+    def reset_parameters(self):
+        self.lin.reset_parameters()
+        self.bias.data.zero_()
 
+    def forward(self,x,edge_index):
+        edge_index,_=add_self_loops(edge_index,num_nodes=x.size(0))
 
+        x=self.lin(x)
 
+        row,col=edge_index
+        deg=degree(col,x.size(0),dtype=x.dtype)
+        deg_inv_sqrt=deg.pow(-0.5)
+        deg_inv_sqrt[deg_inv_sqrt==float('inf')]==0
+        norm=deg_inv_sqrt[row]*deg_inv_sqrt[col]
 
+        out=self.propagate(edge_index,x=x,norm=norm)
 
+        out+=self.bias
 
+        return out
+    
+    def message(self,x_j,norm):
+        return norm.view(-1, 1) * x_j
 
+    
