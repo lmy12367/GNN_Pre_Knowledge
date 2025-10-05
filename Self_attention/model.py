b14199f421242947756341f14231736d61489b57
lmy12367 <lfuturemy@163.com>
2025-10-05 19:40:25 +0800

    model

--- Self_attention/model.py
+++ Self_attention/model.py
@@ -66,7 +66,7 @@ class SAGPooling_Hierarchical(nn.Module):
         self.lin1=nn.Linear(self.hid*2,self.hid)
         self.lin2=nn.Linear(self.hid,self.hid//2)
 
-        self.classifier=nn.Linear(self.hid//2,num_features)
+        self.classifier=nn.Linear(self.hid//2,num_classes)
 
     def forward(self,data):
         x,edge_index,batch=data.x,data.edge_index,data.batch
@@ -76,7 +76,7 @@ class SAGPooling_Hierarchical(nn.Module):
 
         x1=torch.cat([gmp(x,batch),gap(x,batch)],dim=1)
 
-        x=F.relu(self.conv2(x1,edge_index))
+        x=F.relu(self.conv2(x,edge_index))
         x,edge_index,_,batch,perm,score=self.pool(x,edge_index,None,batch)
 
         x2=torch.cat([gmp(x,batch),gap(x,batch)],dim=1)
@@ -89,7 +89,7 @@ class SAGPooling_Hierarchical(nn.Module):
         x=x1+x2+x3
 
         x=F.relu(self.lin1(x))
-        x=F.dropout(x,p=self.dropout_ration,training=self.training)
+        x=F.dropout(x,p=self.dropout_ratio,training=self.training)
         x=F.relu(self.lin2(x))
 
         x=F.log_softmax(x,dim=1)
